fc13f92116c57d860d940c1a6cc9653a4516ebaa
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-18 17:11:49 +0200

    Esercizio finito tranne l'ultimo

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,22 +19,31 @@ namespace csharp_oop_shop
         private int iva;
         */
 
+       
+        
+
+        //PROPERTIES
+
         public string Name { get; set; }
         public string Description { get; set; }
         public float Prezzo { get; set; }
 
-        private int code;
-        private int iva;
-        //PROPERTIES
-
         
+        public int Code { get; }
+             
 
+        private float Iva { get; set; }
 
+        //private string formattedCode;
         //CONSTRUCTOR
         public Prodotto(string name, string description, float prezzo)
         {
-            
-            
+            Name = name;
+            this.Description = description;
+            this.Prezzo = prezzo;
+            Code = RandomGen();
+            Iva = 1.22f;
+            //formattedCode = FormattedNum();
 
         }
 
@@ -42,14 +52,49 @@ namespace csharp_oop_shop
 
         //METHODS
 
-        private string CodeFormatter()
+        private int RandomGen() //questo metodo dovrebbe generare un random int e trasformarlo in string con il pad adatto e poi restituirlo
         {
             Random rnd = new Random();
-            this.code = rnd.Next(1, 100000000);
-
-            string code = this.code.ToString("D8");
-            
-            return code;
+            int codeInt = rnd.Next(1, 100000000);
+            return codeInt;
+           
         }
+
+        public string FormattedNum()
+        {
+            int padding = 0;
+            padding = Code.ToString().Length;
+            padding = 8 - padding;
+            string zeros = "";
+            
+            for ( int i = 0; i < padding; i++ )
+            {
+                zeros += "0";
+
+            }
+
+            zeros = zeros + Code;
+            return zeros;
+
+        }
+
+        public string CodeName()
+        {
+            string codeName = FormattedNum() + Name;
+            return codeName;
+        }
+
+      
+
+        float PrezzoBase()
+        {
+            return Code;
+        }
+
+        float PrezzoIva()
+        {
+            return Code * Iva;
+        }
+        
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using csharp_oop_shop;
+
+Prodotto myProdotto = new Prodotto("pinco" , "pallino", 120.5f);
+
+
+Console.WriteLine(myProdotto.Name);
+Console.WriteLine(myProdotto.Description);
+Console.WriteLine(myProdotto.Prezzo);
+Console.WriteLine(myProdotto.Code);
+Console.WriteLine(myProdotto.FormattedNum());
+Console.WriteLine(myProdotto.CodeName());
+
+Console.WriteLine("Il codice formattato è : " + myProdotto.FormattedNum());
+
